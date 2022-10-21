a3903d940415e7db7af429bf96cd8d4199d3f317
James Demlow <jdemlow@gmail.com>
2022-10-21 17:45:38 -0600

    Add files via upload

---
+++ js/removeSpaces.js
@@ -0,0 +1,8 @@
+function noSpace(x) {
+  return x.split(" ").join("");
+}
+
+console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"));
+console.log(noSpace("8aaaaa dddd r     "));
+
+//www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/javascript
