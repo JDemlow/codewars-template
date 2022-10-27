e3d12a7b05d3fe5a6ee3732ceeebe9e283f1b499
James Demlow <jdemlow@gmail.com>
2022-10-27 15:02:23 -0600

    Add files via upload

---
+++ js/busStop.js
@@ -0,0 +1,13 @@
+// var number = function (arr) {
+//   const sum = arr.reduce((a, b) => a.map((c, i) => c + b[i]));
+//   console.log(sum);
+// };
+
+// number([
+//   [1, 2],
+//   [1, 5],
+// ]);
+
+// couldn't complete, had to look up answer
+
+// https://www.codewars.com/kata/5648b12ce68d9daa6b000099/solutions
