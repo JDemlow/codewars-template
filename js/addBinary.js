e685e5be4d32a8741724ab99884fb10e27a08353
James Demlow <jdemlow@gmail.com>
2022-10-31 15:03:02 -0600

    Add files via upload

---
+++ js/addBinary.js
@@ -0,0 +1,9 @@
+function addBinary(a, b) {
+  let sum = a + b;
+  let str = sum.toString(2);
+  return str;
+}
+
+console.log(addBinary(5, 9));
+
+// https://www.codewars.com/kata/551f37452ff852b7bd000139/solutions/javascript
