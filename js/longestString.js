a9b55b65f42180b44d7b4acf0bb194f852fe5f46
James Demlow <jdemlow@gmail.com>
2022-11-02 21:03:26 -0600

    Add files via upload

---
+++ js/longestString.js
@@ -0,0 +1,20 @@
+// function longest(s1, s2) {
+//   let sorted1 = s1.split("").sort().join("");
+//   let sorted2 = s2.split("").sort().join("");
+//   let conCatStr = sorted1 + sorted2;
+//   return conCatStr;
+// }
+
+// console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+
+// let str = "I like iron man but not super man but i like";
+
+// let set = new Set();
+// let words = str.toLowerCase().split("");
+// words.forEach((e) => {
+//   set.add(e);
+// });
+
+// console.log(set);
+
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac/solutions
