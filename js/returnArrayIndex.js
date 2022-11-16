4955d55222aaf9949591d2940db73985b7561b4c
James Demlow <jdemlow@gmail.com>
2022-11-15 21:08:30 -0700

    Add files via upload

---
+++ js/returnArrayIndex.js
@@ -0,0 +1,7 @@
+var number = function (array) {
+  return array.map(function (line, index) {
+    return index + 1 + ": " + line;
+  });
+};
+
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/solutions
