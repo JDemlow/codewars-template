e97743d4c6106c832a8043d72c5ab88f53d6d009
James Demlow <jdemlow@gmail.com>
2022-11-29 16:24:08 -0700

    Add files via upload

---
+++ js/isUppercase.js
@@ -0,0 +1,9 @@
+let isUpperCase = (str) => {
+  return str === str.toUpperCase();
+};
+
+console.log(isUpperCase("PIZZA"));
+
+//https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/solutions
+
+//review use of 'this' keyword with syntax found in solution
