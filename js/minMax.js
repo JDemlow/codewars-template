082bfef3e9c52f28ede3cdd45e455691fde5f8b4
James Demlow <jdemlow@gmail.com>
2022-12-15 13:02:37 -0700

    Add files via upload

---
+++ js/minMax.js
@@ -0,0 +1,12 @@
+let min = function (list) {
+  return Math.min(...list);
+};
+
+let max = function (list) {
+  return Math.max(...list);
+};
+
+console.log(min([5, 9, 2, 5, 1, 3, 4]));
+console.log(max([5, 9, 2, 5, 1, 13, 3, 4]));
+
+//https://www.codewars.com/kata/577a98a6ae28071780000989/solutions/javascript
