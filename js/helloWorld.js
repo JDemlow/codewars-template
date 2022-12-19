f7e0e2e6dee2077605bff59870f547e26c85491d
James Demlow <jdemlow@gmail.com>
2022-12-18 21:39:59 -0700

    Add files via upload

---
+++ js/helloWorld.js
@@ -0,0 +1,8 @@
+function helloWorld() {
+  let str = "Hello World!";
+  console.log(str);
+}
+
+console.log(helloWorld());
+
+//https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/solutions/javascript
