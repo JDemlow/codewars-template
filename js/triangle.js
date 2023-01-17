e927448c9ef2bc276ba350ea9a5df9a76dd0128e
James Demlow <jdemlow@gmail.com>
2023-01-17 12:34:52 -0700

    Add files via upload

---
+++ js/triangle.js
@@ -0,0 +1,7 @@
+function returnAngle(ang1, ang2) {
+  return 180 - ang1 - ang2;
+}
+
+console.log(returnAngle(60, 60));
+
+//https://www.codewars.com/kata/5a023c426975981341000014/solutions/javascript
